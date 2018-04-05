811e3ac417b253cedb1c3af87acbf4347c8c4ed8
Electronic MakerSpace <38089209+ElectronicMakerSpace@users.noreply.github.com>
2018-04-05 16:09:51 -0500

    Add files via upload

---
+++ Lectura_de_Temperatura.ino
@@ -0,0 +1,36 @@
+/*
+ * Reading Temperature/Lectura de Temperatura
+ * 
+ * Lee la temperatura en centígrado sobre el serial.
+*/
+
+//Se establece el pin de salida del sensor de temperatura LM35
+int lm35Pin = A0;
+
+void setup()
+{
+    //Configurar el puerto serial
+    Serial.begin(9600);
+}
+
+void loop()
+{ 
+    int analogValue;
+    float temperature;
+
+    //Se lee el sensor de temperatura
+    analogValue = analogRead(lm35Pin);
+
+    //Se convierte el valor analógico de 10 bit a celsius
+    temperature = float(analogValue) / 1023;
+    temperature = temperature * 500;
+
+    //Se imprime la temperatura en el monitor serial
+    Serial.print("Temp: ");
+    Serial.print(temperature);
+    Serial.println("C");
+    
+    //Espera de 1 segundo antes de leer la temperatura nuevamente
+    delay(1000);
+}
+
